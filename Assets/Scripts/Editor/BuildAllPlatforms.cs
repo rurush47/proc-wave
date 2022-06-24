71b5792e376a25c4b8e7eef3c0a881a5ada6c5bc
alex <aleksander.rurarz@holo4med.com>
2022-06-24 21:12:07 +0200

    QoL build settings

---
+++ Assets/Scripts/Editor/BuildAllPlatforms.cs
@@ -0,0 +1,15 @@
+using UnityEditor;
+
+public class BuildAllPlatforms 
+{
+    [MenuItem("ProcWave/Build Win+Mac+Lnx")]
+    public static void BuildGame ()
+    {
+        string path = EditorUtility.SaveFolderPanel("Choose Location of Built Game", "", "");
+        string[] levels = new string[] {"Assets/Scenes/MainScene.unity"};
+
+        BuildPipeline.BuildPlayer(levels, path + "/Win/Demo.exe", BuildTarget.StandaloneWindows, BuildOptions.None);
+        BuildPipeline.BuildPlayer(levels, path + "/Mac/Demo.app", BuildTarget.StandaloneOSX, BuildOptions.None);
+        BuildPipeline.BuildPlayer(levels, path + "/Linux/Demo.x86_64", BuildTarget.StandaloneLinux64, BuildOptions.None);
+    }
+}
