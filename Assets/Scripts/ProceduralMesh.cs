620fb4a80c786dedc5fc8072be0618c6c8892d40
Rurek <>
2022-06-24 13:23:32 +0200

    Add wave animation

--- Assets/Scripts/ProceduralMesh.cs
+++ Assets/Scripts/ProceduralMesh.cs
@@ -9,6 +9,8 @@ public class ProceduralMesh : MonoBehaviour
     [SerializeField] private float _waitTime;
     private MeshFilter _meshFilter;
     private Mesh _mesh;
+    private Vector3[] _vertices;
+    private int[] _triangles;
 
     private void Start()
     {
@@ -16,52 +18,58 @@ public class ProceduralMesh : MonoBehaviour
         GenerateMesh();
     }
 
-    private Vector3[] vertices;
-    private int[] triangles;
-    
     private async void GenerateMesh()
     {
         _mesh = new Mesh();
         
-        vertices = new Vector3[(_surfaceData.XSize + 1) * (_surfaceData.ZSize + 1)];
+        _vertices = new Vector3[(_surfaceData.XSize + 1) * (_surfaceData.ZSize + 1)];
         int index = 0;
         for (int z = 0; z <= _surfaceData.ZSize; z++)
         {
             for (int x = 0; x <= _surfaceData.XSize; x++)
             {
-                vertices[index++] = new Vector3(x, 0, z);
+                _vertices[index++] = new Vector3(x, 0, z);
             }
         }
 
-        triangles = new int[_surfaceData.XSize * _surfaceData.ZSize * 6];
+        _triangles = new int[_surfaceData.XSize * _surfaceData.ZSize * 6];
         index = 0;
         for (int z = 0; z < _surfaceData.ZSize; z++)
         {
             for (int x = 0; x < _surfaceData.XSize; x++)
             {
-                triangles[index++] = x + z + z * _surfaceData.XSize;
-                triangles[index++] = x + z + (z + 1) * _surfaceData.XSize + 1;
-                triangles[index++] = x + z + 1 + z * _surfaceData.XSize;
+                _triangles[index++] = x + z + z * _surfaceData.XSize;
+                _triangles[index++] = x + z + (z + 1) * _surfaceData.XSize + 1;
+                _triangles[index++] = x + z + 1 + z * _surfaceData.XSize;
 
                 await Task.Delay(TimeSpan.FromSeconds(_waitTime));
                 
-                triangles[index++] = x + z + 1 + z * _surfaceData.XSize;
-                triangles[index++] = x + z + (z + 1) * _surfaceData.XSize + 1;
-                triangles[index++] = x + z + 1 + (z + 1) * _surfaceData.XSize + 1;
+                _triangles[index++] = x + z + 1 + z * _surfaceData.XSize;
+                _triangles[index++] = x + z + (z + 1) * _surfaceData.XSize + 1;
+                _triangles[index++] = x + z + 1 + (z + 1) * _surfaceData.XSize + 1;
             }
         }
         
-        _mesh.vertices = vertices;
-        _mesh.triangles = triangles;
-        _mesh.RecalculateNormals();
         _meshFilter.mesh = _mesh;
+
+        _mesh.vertices = _vertices;
+        _mesh.triangles = _triangles;
+        _mesh.RecalculateNormals();
     }
 
     private void Update()
     {
-        _mesh.vertices = vertices;
-        _mesh.triangles = triangles;
+        for (int z = 0, i = 0; z <= _surfaceData.ZSize; z++)
+        {
+            for (int x = 0; x <= _surfaceData.XSize; x++, i++)
+            {
+                float y = 3 * Mathf.Sin(Mathf.PI*((float)x/_surfaceData.XSize + (float)z/_surfaceData.ZSize + Time.realtimeSinceStartup));
+                _vertices[i] = new Vector3(x, y, z);
+            }
+        }
+        
+        _mesh.vertices = _vertices;
+        _mesh.triangles = _triangles;
         _mesh.RecalculateNormals();
-        _meshFilter.mesh = _mesh;
     }
 }
